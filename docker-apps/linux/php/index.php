ced87af3c84ab70fd7eaa8e773300f0a35e1e325
n4demo <n.wardle@node4.co.uk>
2022-02-16 13:56:56 +0000

    removed ref to previous

--- docker-apps/linux/php/index.php
+++ docker-apps/linux/php/index.php
@@ -8,12 +8,10 @@
 
 <body>
   <h1>
-  <?php echo "My name here - PHP website running in docker Container for CDW"; ?>
+  <?php echo "My name here - PHP website running in docker Container"; ?>
   </h1>
 
-  <img src="images/cdw.png" />
-
-  <img src="images/php.png" />
+   <img src="images/php.png" />
 
   <iframe src="https://www.liverpoolfc.com/" height="600" width="800" />
 
